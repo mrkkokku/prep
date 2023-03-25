126a6412897dde9d9de48420867925c3dd738e34
mrkkokku <mrkkokku@gmail.com>
2023-03-25 23:42:56 +0900

    2023-03-25

--- python/coding_test/old_06_00_bubbleSort.py
+++
@@ -1,8 +0,0 @@
-array = [ 7, 5, 9, 0, 3, 1, 6, 2, 4, 8 ]
-
-for i in range(len(array)-1):
-    for j in range(0, len(array)-1-i):
-        if array[j] > array[j+1]:
-            array[j], array[j+1] = array[j+1], array[j]
-
-print(array)
