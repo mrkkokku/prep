45f993f3202e339b90b66f997f4595d20b1a2816
mrkkokku <mrkkokku@gmail.com>
2023-06-17 21:08:45 +0900

    2023-06-17

--- Java/coding_test/src/coding_test/Level01.java
+++ Java/coding_test/src/coding_test/Level01.java
@@ -143,10 +143,45 @@ public class Level01 {
         
         return answer;
 	}
+	
+	public static boolean handlingSTR(String s) {
+		/*
+		문자열 s의 길이가 4 혹은 6이고,
+		숫자로만 구성돼있는지 확인해주는 함수,
+		solution을 완성하세요.
+		
+		예를 들어 s가 "a234"이면 False를 리턴하고 "1234"라면 True를 리턴하면 됩니다.
+
+		제한 사항
+		s는 길이 1 이상, 길이 8 이하인 문자열입니다.
+		s는 영문 알파벳 대소문자 또는 0부터 9까지 숫자로 이루어져 있습니다.
+		
+		s	return
+		"a234"	false
+		"1234"	true
+		*/
+		boolean answer = true;
+        
+		if(!(s.length() == 4 || s.length() == 6)) {
+			answer = false;
+		} else {
+			String[] temp = s.split("");
+			
+			for(int i = 0; i < temp.length; i++) {
+				try {
+					int isNum = Integer.valueOf(temp[i]);
+				} catch (Exception e) {
+					answer = false;
+					break;
+				}
+			}
+		}
+		return answer;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("=====시작=====\n");
-		Level01.numToArray(532521);
+		System.out.println(Level01.handlingSTR("a234"));
 		System.out.println("\n======끝======");
 	}
 
