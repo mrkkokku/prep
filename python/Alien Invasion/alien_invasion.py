9f6ccd5221f14239b58ed00142e54394b23cb9e7
mrkkokku <mrkkokku@gmail.com>
2023-07-10 21:30:15 +0900

    2023-07-10

--- python/Alien Invasion/alien_invasion.py
+++ python/Alien Invasion/alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
 
@@ -12,17 +13,25 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
+        ''' FULL SCREEN SETTING
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+        '''
+
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
     
     def run_game(self):
 
         while(True):
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -40,15 +49,25 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
-    
+        elif event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         if event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
          self.screen.fill(self.settings.bg_color)
+         for bullet in self.bullets.sprites():
+             bullet.draw_bullet()  
          self.ship.blitme()
 
          pygame.display.flip()
